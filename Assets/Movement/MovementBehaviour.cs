38d894f65263370d4eb06689841fc3af7c737b18
luxtuu <102391309+luxtuu@users.noreply.github.com>
2024-03-04 16:02:28 -0400

    AnimationMovement

--- Assets/Movement/MovementBehaviour.cs
+++ Assets/Movement/MovementBehaviour.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class MovementBehaviour : MonoBehaviour
 {
     public float motiSpeed;
-    float speedX, speedY;
+    float speedX = 0f;
+    float speedY = 0f;
     Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         speedX = Input.GetAxisRaw("Horizontal") * motiSpeed;
+        animator.SetFloat("Horizontal", Mathf.Abs(speedX));
+
         speedY = Input.GetAxisRaw("Vertical") * motiSpeed;
+        //animator.SetFloat("Vertical", Mathf.Abs(speedY));
         rb.velocity = new Vector2(speedX, speedY);
     }
 }
