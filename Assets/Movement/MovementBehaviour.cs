dad44260e1e6343201f2663e43fb230c0f663aa8
luxtuu <102391309+luxtuu@users.noreply.github.com>
2024-03-04 21:13:52 -0400

    AnimacionAndMovement

--- Assets/Movement/MovementBehaviour.cs
+++ Assets/Movement/MovementBehaviour.cs
@@ -5,24 +5,44 @@ using UnityEngine;
 public class MovementBehaviour : MonoBehaviour
 {
     public float motiSpeed;
-    float speedX = 0f;
-    float speedY = 0f;
-    Rigidbody2D rb;
-    private Animator animator;
+    private float movementX, movementY;
+    private Rigidbody2D rbCharacter;
+    private Vector2 cursoDeMovement;
+    [SerializeField] Animator animi;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
+        rbCharacter = this.GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        speedX = Input.GetAxisRaw("Horizontal") * motiSpeed;
-        animator.SetFloat("Horizontal", Mathf.Abs(speedX));
+        EncierroDePulso();
+        animi.SetFloat("Horizontal", cursoDeMovement.x);
+        animi.SetFloat("Vertical", cursoDeMovement.y);
+        // speedX = Input.GetAxisRaw("Horizontal") * motiSpeed;
+        // animator.SetFloat("Horizontal", Mathf.Abs(speedX));
 
-        speedY = Input.GetAxisRaw("Vertical") * motiSpeed;
+        //  speedY = Input.GetAxisRaw("Vertical") * motiSpeed;
         //animator.SetFloat("Vertical", Mathf.Abs(speedY));
-        rb.velocity = new Vector2(speedX, speedY);
+        //  rb.velocity = new Vector2(speedX, speedY);
+    }
+
+    private void FixedUpdate()
+    {
+        MoveOfCharacter();
+    }
+
+    void EncierroDePulso()
+    {
+        movementX = Input.GetAxisRaw("Horizontal");
+        movementY = Input.GetAxisRaw("Vertical");
+        cursoDeMovement = new Vector2(movementX, movementY);
+    }
+
+    void MoveOfCharacter()
+    {
+        rbCharacter.velocity = new Vector2(cursoDeMovement.x * motiSpeed, cursoDeMovement.y * motiSpeed);
     }
 }
