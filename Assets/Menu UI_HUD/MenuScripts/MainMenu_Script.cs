9b120225891bc0a8658f8ef8ff8f1bc5d9fdb63d
Kevalb22 <koalbino@gmail.com>
2024-03-12 17:45:35 -0400

    Menus
    Se completo los menus con su funcionalidad
    
    Faltaria el slider de volumen que se anadaria funcialidad cuando trabajemos con audio

--- Assets/Menu UI_HUD/MenuScripts/MainMenu_Script.cs
+++ Assets/Menu UI_HUD/MenuScripts/MainMenu_Script.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.SceneManagement
+using UnityEngine.SceneManagement;
 
 public class MainMenu_Script : MonoBehaviour
 {
-    /*public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }*/
+    }
 
     public void QuitGame()
     {
