e94599d02ff2bb81242111b4049cdf9c5b581711
Kevalb22 <koalbino@gmail.com>
2024-03-12 17:03:38 -0400

    Menus
    Added Main/Options Menu Scene
    (Initial Set up)

---
+++ Assets/Menu UI_HUD/MenuScripts/MainMenu_Script.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManagement
+
+public class MainMenu_Script : MonoBehaviour
+{
+    /*public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }*/
+
+    public void QuitGame()
+    {
+        Debug.Log("You have closed the game.");
+        Application.Quit();
+    }
+}
---
+++ Assets/Menu UI_HUD/MenuScripts/OptionsMenu_Script.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu_Script : MonoBehaviour
+{
+    public void SetVolume (float volume)
+    {
+        Debug.Log("Volume has changed");
+    }
+}
