8533f90ac63fd0a694a5f971ef5a6588f1e0c5fe
luxtuu <102391309+luxtuu@users.noreply.github.com>
2024-03-12 20:04:42 -0400

    HealthHeartBar

--- Assets/Scripts/HealthHeart.cs
+++ Assets/Scripts/HealthHeart.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthHeart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int vida;
+    public int numDeVidas;
 
-    // Update is called once per frame
+    public Image[] vidas;
+    public Sprite vidaCompleta;
+    public Sprite vidaVacia;
+
     void Update()
     {
-        
+        for(int i = 0; i < vidas.Length; i++)
+        {
+            if(vida > numDeVidas)
+            {
+                vida = numDeVidas;
+            }
+
+            if (i < numDeVidas)
+            {
+                vidas[i].sprite = vidaCompleta;
+            }
+            else
+            {
+                vidas[i].sprite = vidaVacia;
+            }
+
+            if(i < numDeVidas)
+            {
+                vidas[i].enabled = true;
+            }
+            else
+            {
+                vidas[i].enabled = false;
+            }
+        }
     }
 }
